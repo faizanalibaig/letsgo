e79a43a86884ab0c6b4fc9f0baa6de153f1d777c
faizanalibaig <riazfiaz6181@gmail.com>
2026-02-20 19:28:58 +0500

    re-implementing project

--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"go-backend/configs/database"
-	"go-backend/internal/user"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
 func main() {
-	r := gin.Default()
-	database.DbConnection()
-
-	r.GET("/health-check", user.Ping)
-	r.POST("/add-user", user.AddUser)
-	r.GET("/get-users", user.GetUsers)
-
-	err := r.Run(":8080")
-
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("let's go")
 }
