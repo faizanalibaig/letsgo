8c8587515bfc52ed3186d233c5e12b88a13fe779
faizanalibaig <riazfiaz6181@gmail.com>
2026-02-21 23:31:10 +0500

    JWT setup: in-progress

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"letsgo/database"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
 var pool *pgxpool.Pool
+var secret = []byte("your-secret-key")
 
 func main() {
 	var err error
@@ -23,5 +26,27 @@ func main() {
 		panic(err)
 	}
 
+	token, err := GenerateToken("exampleUser")
+	if err != nil {
+		fmt.Println("Error generating token:", err)
+	} else {
+		fmt.Println("Generated Token:", token)
+	}
+
 	defer database.Close()
+}
+
+func GenerateToken (user string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user": user,
+		"time": time.Now().Unix(),
+	})
+
+	tokenString, err := token.SignedString(secret)
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
 }
